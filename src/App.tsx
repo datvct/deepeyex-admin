0f3b71d01399b3b6796851124cf75cd96d370caf
datvct <vancongthanhdata10@gmail.com>
2025-10-13 00:49:35 +0700

    feat: integrate socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import { Navigator } from "./navigator/navigator";
+import { WebSocketProvider } from "./shares/contexts/WebSocketContext";
+
 function App() {
   return (
-    <>
+    <WebSocketProvider>
       <Navigator />
-    </>
+    </WebSocketProvider>
   );
 }
 
